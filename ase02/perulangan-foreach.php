f5ffdf0a89631f926a264b5773520c5bdc88d9c9
IQBAL FARHAN SYUHADA <53584619+iqbalfarhan@users.noreply.github.com>
2023-11-18 09:19:48 +0800

    Create perulangan-foreach.php

---
+++ ase02/perulangan-foreach.php
@@ -0,0 +1,18 @@
+<?php
+
+$mahasiswa = [
+    "Budi Setiawan",
+    "Nisa Permata",
+    "Rizky Pratama",
+    "Indah Sari",
+    "Arif Saputra",
+    "Dina Septiani",
+    "Agus Wijaya",
+    "Siti Rahayu",
+    "Fajar Nugraha",
+    "Dewi Anggraini",
+];
+
+foreach ($mahasiswa as $nama) {
+    echo "halo perkenalkan saya {$nama} \n";
+}
