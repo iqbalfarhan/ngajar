8081202a05546a698e7af8a740224571b263a111
IQBAL FARHAN SYUHADA <53584619+iqbalfarhan@users.noreply.github.com>
2023-11-18 09:08:28 +0800

    Update function.php

--- ase02/pertemuan3dan4/function.php
+++ ase02/pertemuan3dan4/function.php
@@ -1,37 +1,31 @@
 <?php
 
 
-//pembuatan function diawali dengan keyword "function" diikuti dengan nama fungction yang diinginkan
 function penjumlahan($a, $b)
 {
     return $a + $b;
 }
-echo "penjumlahan dari 3 dan 4 adalah : " . penjumlahan(3, 4) . "\n";
-
-//==================================
 
 function pengurangan($a, $b)
 {
     return $a - $b;
 }
-echo "pengurangan dari 7 dan 2 adalah : " . pengurangan(7, 2) . "\n";
-
-//==================================
 
 function pengalian($a, $b)
 {
     return $a * $b;
 }
-echo "pengalian dari 23 dan 1 adalah : " . pengalian(23, 1) . "\n";
-
-//==================================
 
 function pembagian($a, $b)
 {
     return $a / $b;
 }
+
+// cara menggunakan fungsi
+
+echo "penjumlahan dari 3 dan 4 adalah : " . penjumlahan(3, 4) . "\n";
+echo "pengurangan dari 7 dan 2 adalah : " . pengurangan(7, 2) . "\n";
+echo "pengalian dari 23 dan 1 adalah : " . pengalian(23, 1) . "\n";
 echo "pembagian dari 3 dan 4 adalah : " . pembagian(3, 4) . "\n";
-
-
 
 // jalankan dengan php function.php
