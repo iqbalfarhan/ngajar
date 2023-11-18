e7fd684f3265b211c06300d61a29be7c4113ce37
IQBAL FARHAN SYUHADA <53584619+iqbalfarhan@users.noreply.github.com>
2023-11-18 08:56:01 +0800

    Create function.php

---
+++ ase02/pertemuan3dan4/function.php
@@ -0,0 +1,37 @@
+<?php
+
+
+//pembuatan function diawali dengan keyword "function" diikuti dengan nama fungction yang diinginkan
+function penjumlahan($a, $b)
+{
+    return $a + $b;
+}
+echo "penjumlahan dari 3 dan 4 adalah : " . penjumlahan(3, 4) . "\n";
+
+//==================================
+
+function pengurangan($a, $b)
+{
+    return $a - $b;
+}
+echo "pengurangan dari 7 dan 2 adalah : " . pengurangan(7, 2) . "\n";
+
+//==================================
+
+function pengalian($a, $b)
+{
+    return $a * $b;
+}
+echo "pengalian dari 23 dan 1 adalah : " . pengalian(23, 1) . "\n";
+
+//==================================
+
+function pembagian($a, $b)
+{
+    return $a / $b;
+}
+echo "pembagian dari 3 dan 4 adalah : " . pembagian(3, 4) . "\n";
+
+
+
+// jalankan dengan php function.php
