899cec85f10bf121f9e9f7e68dc2d0d5d897e653
IQBAL FARHAN SYUHADA <53584619+iqbalfarhan@users.noreply.github.com>
2023-11-18 08:49:15 +0800

    Create variable.php

---
+++ ase02/pertemuan2/variable.php
@@ -0,0 +1,11 @@
+<?php
+
+// variable diawali dengan tanda dollar $;
+
+$nama = "iqbal farhan syuhada";
+
+echo "hallo";
+echo $name;
+
+// hasil yang akan tampil bila di jalankan adalah
+// hallo iqbal farhan syuhada
