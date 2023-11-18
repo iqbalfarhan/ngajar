f37c5566a7e1340632135789255c1c0e117fede8
IQBAL FARHAN SYUHADA <53584619+iqbalfarhan@users.noreply.github.com>
2023-11-18 09:08:48 +0800

    Update function.php

--- ase02/pertemuan3dan4/function.php
+++ ase02/pertemuan3dan4/function.php
@@ -27,5 +27,3 @@ echo "penjumlahan dari 3 dan 4 adalah : " . penjumlahan(3, 4) . "\n";
 echo "pengurangan dari 7 dan 2 adalah : " . pengurangan(7, 2) . "\n";
 echo "pengalian dari 23 dan 1 adalah : " . pengalian(23, 1) . "\n";
 echo "pembagian dari 3 dan 4 adalah : " . pembagian(3, 4) . "\n";
-
-// jalankan dengan php function.php
